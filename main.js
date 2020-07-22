2a55e0df2bc89bc8f9e6624bacec34edf8d8d9a1
John Raymond <jray014@gmail.com>
2020-07-22 12:01:31 -0400

    Start button function and click complete.

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 const player = document.querySelector('.player');
-const maze = document.querySelector('.maze');
-const mazeTile = document.querySelectorAll('.maze-tile');
-const startButton = document.querySelector('.begin');
-x.addEventListener('click', y);
+// const maze = document.querySelector('.maze');
+// const mazeTile = document.querySelectorAll('.maze-tile');
+const startButton = document.querySelector('.start');
 
 function enterTheMaze() {
-    const tile10 = document.querySelector('.10');
+    const tile10 = document.querySelector('#ten');
+    // const startButton = document.querySelector('.start');
+    const maze = document.querySelector('.maze');
     tile10.append(player);
     startButton.remove();
     maze.style.height = '98%';
+    // player.style.
+    console.log("it works")
 }
 
 function move() {
-
+    
 }
-startButton.addEventListener('click', enterTheMaze());
+startButton.addEventListener('click', enterTheMaze);
+
+// x.addEventListener('click', move());
 //have player move in each tile of the div.
 //every folk causes a click event.
