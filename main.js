dd22b51251a8275752d79949cb0e196c3e3a84f5
John Raymond <jray014@gmail.com>
2020-07-24 11:20:14 -0400

    worked on win function, added reset button, created a code scrap file.

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
 const player = document.querySelector('.player');
 const board = document.querySelector('body');
-// const maze = document.querySelector('.maze');
-// const mazeTile = document.querySelectorAll('.maze-tile');
+const maze = document.querySelector('.maze');
+const mazeTile = document.querySelectorAll('.maze-tile');
 const startButton = document.querySelector('.start');
+const tile15 = document.querySelector('#fifthteen');
+const tile6 = document.querySelector('#six');
+const tile8 = document.querySelector('#eight');
+const tile9 = document.querySelector('#nine');
+const tile10 = document.querySelector('#ten');
+const tile14 = document.querySelector('#fourteen');
+const tile19 = document.querySelector('#nineteen');
+const tile20 = document.querySelector('#twenty');
+
+const moveConditions = [
+    ['twenty'],
+    ['fifthteen','nineteen'],
+    ['twenty','fourteen'],
+    ['nineteen','nine'],
+    ['fourteen','ten','eight'],
+    ['nine'],
+    ['nine','seven'],
+    ['eight','six'],
+]
 
 function enterTheMaze() {
-    const tile10 = document.querySelector('#fifthteen');
+//startTile
+    // const tile15 = document.querySelector('#fifthteen');
     // const startButton = document.querySelector('.start');
-    const maze = document.querySelector('.maze');
-    tile10.append(player);
+    // const maze = document.querySelector('.maze');
+    tile15.append(player);
     startButton.remove();
     maze.style.height = '98%';
     // player.style.
     // console.log("it works")
     const resetButton = document.createElement('button');
     resetButton.innerHTML = 'Reset Maze';
+    board.append(resetButton);
 }
 
-function move() {
-    
-}
+function move(tile) {
+    // if (tile15.children.includes(player) == true) {
+        tile.append(player);
+    }
+
 
 function win() {
-    if(player) {}
+    if(player.parentElement = tile6) {
+        alert('You made it out of the Maze!');
+        reset();
+    }
     //target eventlistener to tile 6
 }
 
@@ -30,8 +56,13 @@ function reset() {
     
 }
 
+// if (player.parentElement = tile15) {
+//         console.log('yes')
+//     }
+
+
 startButton.addEventListener('click', enterTheMaze);
 
 // x.addEventListener('click', move());
 //have player move in each tile of the div.
-//every folk causes a click event.
+// every folk causes a click event.
