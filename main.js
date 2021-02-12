c9ca1129a9dddf36d8f1ab5584b2c4f122f9c2d8
John Raymond <jray014@gmail.com>
2021-02-12 00:10:27 -0500

    Collision Problems

--- main.js
+++ main.js
@@ -5,54 +5,102 @@ const startButton = document.querySelector('.start');
 const resetButton = document.createElement('button');
 const ctx = maze.getContext("2d"); 
 
+
+class Walls {
+    constructor(fx, fy, sx, sy) {
+        this.fx = fx,
+        this.fy = fy,
+        this.sx = sx,
+        this.sy = sy
+    } 
+    render() {
+        ctx.lineTo(this.fx, this.fy);
+    }
+    collision() {
+        // ctx.isPointInPath(this.x, this.y); //isPointInStroke
+        const wallsCollision = {
+            x: [],
+            y: []
+        }
+        for (let i = this.sx; i <= this.fx; i++) {
+            wallsCollision.x.push(i);
+        }
+        for (let i = this.sy; i <= this.fy; i++) {
+            wallsCollision.y.push(i);
+        }
+        // rx = this.fx - this.sx;
+        // ry = this.fy - this.sy;
+        // wallsCollision.push({
+            //     x: rx,
+            //     y: ry
+            // });
+        return wallsCollision;
+    }
+}
+
+
 const playerImg = new Image();
 playerImg.src = "./assets/rat_face_icon.png";
 
 const player = {
-    x: 50,
-    y: 700,
+    x: 66,
+    y: 702,
     render: function() {
         ctx.drawImage(playerImg, this.x, this.y, 25, 20);
     },
     move: function(pr) {
         if (pr.key === "w") {
-            for (let i=0; i<wallsCollision.length; i++) {
-                if (this.y == wallsCollision[i].y) {
-                    console.log(wallsCollision[i]);
-                    this.y = 0;
+            for (let i=0; i<wall6.collision().y.length; i++) {
+                if (this.y === wall6.collision().y[i]) {
+                    console.log("Bang");
+                    this.y -= 0;
+                    break;
                 } else {
-                console.log(wallsCollision);
-                this.y -= 15;
+                    this.y -= 12;
                 }
             }
         } else if (pr.key === "s") {
-        	this.y += 15
+           this.y += 12;
         } else if (pr.key === "a") {
-        	this.x -= 15
+            this.x -= 1;
         } else if (pr.key === "d") {
-        	this.x += 15
+            
+            // if (wallsCheck() == 0) {
+            //     this.x += 0;
+            //     console.log(wallsCheck());
+            // } else if (wallsCheck() == 1) {
+            //     this.x += 1;
+            //     console.log(wallsCheck());
+            // }
+            // for (let i=0; i < maze1Walls.length; i++){
+            //     if ((this.x >= maze1Walls[i].sx && this.x <= maze1Walls[i].fx) || (this.y >= maze1Walls[i].sy && this.y <= maze1Walls[i].fy)){
+            //         console.log('Collide');
+            //         this.x += 0;
+            //     } else if ((this.x <= maze1Walls[i].sx || this.x >= maze1Walls[i].fx) && (this.y <= maze1Walls[i].sy || this.y >= maze1Walls[i].fy)) {
+            //         this.x += 1;
+            //     }
+            // }
         }
         game.render();
     }    
 }
 
-class Walls {
-    constructor(x, y) {
-        this.x = x,
-        this.y = y
-    } 
-    render() {
-        ctx.lineTo(this.x, this.y);
-    }
-    collision() {
-        ctx.isPointInPath(this.x, this.y); //isPointInStroke
-        wallsCollision.push({
-        x: this.x,
-        y: this.y
-        });
+document.addEventListener('keypress', pr => player.move(pr));
+
+//Update board
+const game = {
+    render: function() {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, maze.width, maze.height);
+// ctx.clearRect(0, 0, player.x, player.y);
+        generateMaze();
+        player.render();
+        //add win point
     }
 }
-const wallsCollision = [];
+
+
+
 
 function enterTheMaze() {
     playerIcon.remove();
@@ -77,22 +125,25 @@ function reset() {
     ctx.clearRect(0, 0, maze.width, maze.height);
 }
 
-resetButton.addEventListener('click', reset)
+resetButton.addEventListener('click', reset);
 
 
-let wall1 = new Walls(126, 252);
-let wall2 = new Walls(378, 252);
-let wall3 = new Walls(630, 126);
-let wall4 = new Walls(630, 504);
-let wall5 = new Walls(504, 504);
-let wall6 = new Walls(0, 378);
-let wall7 = new Walls(378, 630);
-let wall8 = new Walls(126, 504);
-let wall9 = new Walls(252, 504);
-let wall10 = new Walls(252, 630);
-let wall11 = new Walls(630, 630);
-let wall12 = new Walls(630, 756);
-let wall13 = new Walls(504, 256);
+const maze1Walls = []
+let wall1 = new Walls(126, 252, 126, 0);
+let wall2 = new Walls(378, 252, 126, 252);
+let wall3 = new Walls(630, 126, 252, 126);
+let wall4 = new Walls(630, 504, 630, 126);
+let wall5 = new Walls(504, 504, 630, 504);
+let wall6 = new Walls(0, 378, 630, 378);
+let wall7 = new Walls(378, 630, 378, 378);
+let wall8 = new Walls(126, 504, 126, 756);
+let wall9 = new Walls(252, 504, 126, 504);
+let wall10 = new Walls(252, 630, 252, 504);
+let wall11 = new Walls(630, 630, 504, 630);
+let wall12 = new Walls(630, 756, 630, 630);
+let wall13 = new Walls(504, 256, 504, 126);
+maze1Walls.push(wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13);
+
 //generate maze walls
 function generateMaze() {
     ctx.beginPath();
@@ -131,18 +182,40 @@ function generateMaze() {
     // ctx.lineTo(504, 252);
     ctx.stroke();
 }
+
+//check function
+function wallsCheck() {
+    let checkY = ""
+    let checkX = ""
+    for (let i=0; i<maze1Walls.lenth; i++){
+        for (let j=0; j<maze1Walls[i].collision().y.length; j++) {
+           if (player.y == maze1Walls[i].collision().y[j]) {
+               checkY = "true";
+           } else {
+               checkY = "false";
+           }
+           console.log(checkY);
+        }
+        for (let k=0; k<maze1Walls[i].collision().x.length; k++) {
+            if (player.x == maze1Walls[i].collision().x[k]) {
+                checkX = "true";
+            } else {
+                checkX = "false";
+            }
+            console.log(checkX);
+        }
+    }
+    if (checkY == "true" && checkX == "true") {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 //generate collision on walls
-    //Ties into maze wall generation.
+//Ties into maze wall generation.
     //Single array with line coordinates on the canvas that loops for collision.
 
-function addWallsCollision(wx, wy) {
-    ctx.lineTo(wx, wy);
-    ctx.isPointInPath(wx, wy); //isPointInStroke
-    walls.push({
-        x: wx,
-        y: wy
-    });
-}
 wall1.collision()
 wall2.collision()
 wall3.collision()
@@ -156,27 +229,38 @@ wall10.collision()
 wall11.collision()
 wall12.collision()
 wall13.collision()
-
-//Update board
-const game = {
-    render: function() {
-        ctx.beginPath();
-        ctx.clearRect(0, 0, maze.width, maze.height);
-        // ctx.clearRect(0, 0, player.x, player.y);
-        generateMaze();
-        player.render();
-        //add win point
-    }
-}
-
-document.addEventListener('keypress', pr => player.move(pr));
-
-
-
+    
+    
+    
+    
+    
+    
+    
 //if player x and y match wall x and y then stop movement. 
 //loop through walls each time to check. 
 //make walls class and add to it each time a new wall is created in a template.
-
+    
 //collision on walls
 //collision on edges
 //collision on win point
+
+
+
+
+    // function addWallsCollision(wx, wy) {
+    //     ctx.lineTo(wx, wy);
+    //     ctx.isPointInPath(wx, wy); //isPointInStroke
+    //     walls.push({
+    //         x: wx,
+    //         y: wy
+    //     });
+    // }
+    // for (let i=0; i<wallsCollision.length; i++) {
+            //     if (this.x == wallsCollision[i].x) {
+            //         console.log(wallsCollision[i]);
+            //         this.x -= 0;
+            //     } else {
+            //         console.log(wallsCollision);
+                    // this.x -= 1;
+            //     }
+            // }
